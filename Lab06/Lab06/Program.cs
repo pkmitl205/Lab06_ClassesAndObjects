6a2650f065a45bf09b7ff0847e05f52beee34924
pkmitl205 <58030205@kmitl.ac.th>
2017-10-31 13:14:58 +0700

    [class Student] เปลี่ยนจาก public เป็น private

--- Lab06/Lab06/Program.cs
+++ Lab06/Lab06/Program.cs
@@ -18,9 +18,8 @@ namespace Lab06
     }
     class Student
     {
-        public string Name;
-        public string ID;
-        public float GPA;
+        private string Name;
+        private string ID;
+        private float GPA;
     }
-
 }
