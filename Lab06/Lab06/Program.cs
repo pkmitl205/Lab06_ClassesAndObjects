845b234f7ce211599b924db87851451b8c5d7318
pkmitl205 <58030205@kmitl.ac.th>
2017-11-02 19:53:31 +0700

    การพ้องรูปแบบ dynamic polymorphism

--- Lab06/Lab06/Program.cs
+++ Lab06/Lab06/Program.cs
@@ -6,38 +6,40 @@ namespace Lab06
     {
         static void Main(string[] args)
         {
-            Vector v1 = new Vector(1, 4);
-            Vector v2 = new Vector(8, 5);
-            Vector v3 = v1 + v2;
-            Console.WriteLine("vector v1 = " + v1);
-            Console.WriteLine("vector v2 = " + v2);
-            Console.WriteLine("vector v3 = " + v3);
-            Console.WriteLine("-v3 = " + (-v3));
+            Person p = new Person();
+            p.SayHi();
+            Student s = new Student();
+            s.SayHi();
+            Person sp = new Student();
+            sp.SayHi();
+            Teacher t = new Teacher();
+            t.SayHi();
+            Person tp = new Teacher();
+            tp.SayHi();
             Console.ReadKey();
         }
     }
-    class Vector
+    class Person
     {
-        public Vector(int x, int y)
+        public virtual void SayHi()
         {
-            this.X = x;
-            this.Y = y;
+            Console.WriteLine("Hi I'm a person");
         }
-        public int X { get; set; }  // X - accessor (property)
-        public int Y { get; set; }  // Y - accessor (property)
+    }
 
-        public override string ToString()
+    class Student : Person
+    {
+        public override void SayHi()
         {
-            return string.Format("({0},{1})", X, Y);
+            Console.WriteLine("Hi I'm a student");
         }
-        // All operator overloads must be public and static.
-        public static Vector operator +(Vector v1, Vector v2) //binary operator +
+    }
+
+    class Teacher : Person
+    {
+        public new void SayHi()
         {
-            return new Vector(v1.X + v2.X, v1.Y + v2.Y);
-        }
-        public static Vector operator -(Vector v1)  //unary operator -
-        {
-            return new Vector(-v1.X, -v1.Y);
+            Console.WriteLine("Hi I'm a teacher");
         }
     }
 
